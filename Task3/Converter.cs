ddb2a1fbc568f6cd0d6d9300fadea47f205e7bcc
YevhenYeris <58602923+YevhenYeris@users.noreply.github.com>
2020-10-01 02:03:31 +0300

    Завдання 3

---
+++ Task3/Converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    class Converter
+    {
+        Dictionary<string, Dictionary<string, double>> rates = 
+            new Dictionary<string, Dictionary<string, double>>();
+
+        public Converter(double usd, double eur)
+        {
+            rates.Add("uah", new Dictionary<string, double>());
+            rates["uah"].Add("uah", 1);
+            rates["uah"].Add("usd", 1 / usd);
+            rates["uah"].Add("eur", 1 / eur);
+
+            rates.Add("usd", new Dictionary<string, double>());
+            rates["usd"].Add("usd", 1);
+            rates["usd"].Add("eur", eur / usd);
+            rates["usd"].Add("uah", usd);
+
+            rates.Add("eur", new Dictionary<string, double>());
+            rates["eur"].Add("eur", 1);
+            rates["eur"].Add("usd", usd / eur);
+            rates["eur"].Add("uah", eur);
+        }
+
+        public double Convert(string from, string to, double val)
+        {
+            return rates[from][to] * val;
+        }
+    }
+
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(28.1825, 32.8429);
+
+            string from = "";
+            string to = "";
+            double value = 0;
+
+            Console.WriteLine("Конвертувати з: ");
+            from = Console.ReadLine();
+
+            Console.WriteLine("Конвертувати в: ");
+            to = Console.ReadLine();
+
+            Console.WriteLine("Введiть суму: ");
+            value = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(converter.Convert(from, to, value));
+        }
+    }
+}
